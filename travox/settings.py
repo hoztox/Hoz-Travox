136a1eb15047e2453b02e66c2ca93c811f76ad51
shadiyaali <shadiyackn@gmail.com>
2025-02-06 16:49:16 +0530

    admin section

--- travox/settings.py
+++ travox/settings.py
@@ -136,20 +136,15 @@ SIMPLE_JWT = {
 
 WSGI_APPLICATION = 'travox.wsgi.application'
 
-
- 
-
 DATABASES = {
     'default': {
-        'ENGINE': config('DATABASE_ENGINE'),
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+ 
 
+ 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -188,7 +183,7 @@ CORS_ALLOW_ALL_METHODS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "http://localhost:5173",
-    
+    "https://main.d1b8tm0sli7eti.amplifyapp.com"
    
 ]
 # Static files (CSS, JavaScript, Images)
